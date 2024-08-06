f24a9c7e5ded1b0169cbdafc60a5ce76f2c0101b
Dmitri Zavadski <dzdimati@gmail.com>
2024-08-06 14:28:20 +0300

    fix lambda permissions; fix get table by id

--- task10/src/lambdas/api_handler/handler.py
+++ task10/src/lambdas/api_handler/handler.py
@@ -240,7 +240,7 @@ class ApiHandler(AbstractLambda):
                     return self.get_tables()
                 elif method == "POST":
                     return self.post_tables(json.loads(event['body']))
-            elif event["resource"] == "/tables/{tableId}":
+            elif event["resource"] == "/tables/{tablesId}":
                 table_id = int(event['path'].split('/')[-1])
                 return self.get_table_by_id(table_id)
             elif path == "reservations":
